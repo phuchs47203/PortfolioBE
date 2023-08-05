a327a5c56bd235235556612b6dfe43ecfa6fb543
phuchs47203 <21522479@gm.uit.edu.vn>
2023-08-05 09:33:56 +0700

    new

---
+++ src/main/java/com/example/Portfolio/PortfolioBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioBeApplication.class, args);
+	}
+
+}
